de2b27893f11a4eff13b7ea099b96fb8942fedf2
Cassiano Maia <cassmala@gmail.com>
2017-12-08 09:23:58 -0200

    Carro do jogador se movendo e flickering resolvido.

--- game.asm
+++ game.asm
@@ -169,6 +169,75 @@ main PROC
 
 ;Estrutura do menu principal
 Main_Menu:
+	call draw_menu
+;instruções para movimento do menu
+	mov eax, 50
+	call Delay
+	call ReadKey
+	jz exitcmp
+	cmp dx, VK_ESCAPE
+	je finish
+	cmp dx, VK_RETURN
+	jne nextcmp
+	cmp menuctrl, 0
+	je gameloop
+	cmp menuctrl, 3
+	je finish
+nextcmp:
+	cmp al, 119
+	jne elsecmp
+	cmp menuctrl, 0
+	je  exitcmp
+	add menuctrl, -1
+	jmp exitcmp
+elsecmp:
+	cmp al, 115
+	jne exitcmp
+	cmp menuctrl , 3
+	je exitcmp
+	add menuctrl, 1
+exitcmp:
+	mov eax, 50
+	call Delay
+	jmp Main_Menu
+;Fim da estrutura do menu principal
+	
+;Inicio do gameloop
+gameloop:
+	call Clrscr
+gameloop_inner:
+	call draw_player
+	mov eax, 50
+	call Delay
+	call ReadKey
+	jz exitcmp_g
+	cmp dx, VK_ESCAPE
+	je finish
+nextcmp_g:
+	cmp al, 97
+	jne elsecmp_g
+	cmp carcontrol, 0
+	je  exitcmp_g
+	add carcontrol, -1
+	jmp exitcmp_g
+elsecmp_g:
+	cmp al, 100
+	jne exitcmp_g
+	cmp carcontrol , 3
+	je exitcmp_g
+	add carcontrol, 1
+exitcmp_g:
+	call draw_game
+	mov eax, 50
+	call Delay
+	jmp gameloop_inner
+	;limpando a matriz da rua e colocando o carrinho na posição
+	
+Finish:
+	exit
+main ENDP
+
+draw_menu PROC
 	call Clrscr
 	call Crlf
 	mov  edx,OFFSET menu01
@@ -231,7 +300,7 @@ Menu2:
     call WriteString
 Menu3:
 	cmp menuctrl, 3
-	jne LookForKey
+	jne exit_draw_menu
 	mov edx, OFFSET menu06
 	call WriteString
 	call Crlf
@@ -243,46 +312,450 @@ Menu3:
 	call Crlf
 	mov  edx,OFFSET menu09h
     call WriteString
+exit_draw_menu:
+	ret
+draw_menu ENDP
+
+draw_player PROC
+	cld
+	mov ecx, LENGTHOF game104
+	mov esi, OFFSET game104
+	mov edi, OFFSET game130
+	rep movsd
+	cld
+	mov ecx, LENGTHOF game104
+	mov esi, OFFSET game104
+	mov edi, OFFSET game131
+	rep movsd
+	cld
+	mov ecx, LENGTHOF game104
+	mov esi, OFFSET game104
+	mov edi, OFFSET game132
+	rep movsd
+	cld
+	mov ecx, LENGTHOF game104
+	mov esi, OFFSET game104
+	mov edi, OFFSET game133
+	rep movsd
 	
-LookForKey:
-	mov eax, 50
-	call Delay
-	call ReadKey
-	jz exitcmp
-	cmp dx, VK_ESCAPE
-	je finish
-	cmp dx, VK_RETURN
-	jne nextcmp
-	cmp menuctrl, 0
-	je gameloop
-	cmp menuctrl, 3
-	je finish
-nextcmp:
-	cmp al, 119
-	jne elsecmp
-	cmp menuctrl, 0
-	je  exitcmp
-	add menuctrl, -1
-	jmp exitcmp
-elsecmp:
-	cmp al, 115
-	jne exitcmp
-	cmp menuctrl , 3
-	je exitcmp
-	add menuctrl, 1
-exitcmp:
-	mov eax, 50
-	call Delay
-	jmp Main_Menu
-;Fim da estrutura do menu principal
+	mov ecx, OFFSET game130
+	cmp carcontrol, 0
+	jne pos1
+	mov ebx, 205
+	add ecx, 5
+	mov BYTE PTR [ecx], bl
+	mov ebx, 201
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 187
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+	mov ecx, OFFSET game131
+	mov ebx, 72
+	add ecx, 5
+	mov BYTE PTR [ecx], bl
+	mov ebx, 186
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 186
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+	mov ecx, OFFSET game132
+	mov ebx, 205
+	add ecx, 5
+	mov BYTE PTR [ecx], bl
+	mov ebx, 204
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 185
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+	mov ecx, OFFSET game133
+	mov ebx, 205
+	add ecx, 5
+	mov BYTE PTR [ecx], bl
+	mov ebx, 200
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 188
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
 	
-;Inicio do gameloop
-gameloop:
+pos1:
+	cmp carcontrol, 1
+	jne pos2
+	mov ecx, OFFSET game130
+	mov ebx, 205
+	add ecx, 14
+	mov BYTE PTR [ecx], bl
+	mov ebx, 201
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 187
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+	mov ecx, OFFSET game131
+	mov ebx, 72
+	add ecx, 14
+	mov BYTE PTR [ecx], bl
+	mov ebx, 186
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 186
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+	mov ecx, OFFSET game132
+	mov ebx, 205
+	add ecx, 14
+	mov BYTE PTR [ecx], bl
+	mov ebx, 204
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 185
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+	mov ecx, OFFSET game133
+	mov ebx, 205
+	add ecx, 14
+	mov BYTE PTR [ecx], bl
+	mov ebx, 200
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 188
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+	
+pos2:
+	cmp carcontrol, 2
+	jne pos3
+	mov ebx, 205
+	add ecx, 25
+	mov BYTE PTR [ecx], bl
+	mov ebx, 201
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 187
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+	mov ecx, OFFSET game131
+	mov ebx, 72
+	add ecx, 25
+	mov BYTE PTR [ecx], bl
+	mov ebx, 186
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 186
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+	mov ecx, OFFSET game132
+	mov ebx, 205
+	add ecx, 25
+	mov BYTE PTR [ecx], bl
+	mov ebx, 204
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 185
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+	mov ecx, OFFSET game133
+	mov ebx, 205
+	add ecx, 25
+	mov BYTE PTR [ecx], bl
+	mov ebx, 200
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 188
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+pos3:
+	cmp carcontrol, 3
+	jne end_draw
+	mov ebx, 205
+	add ecx, 34
+	mov BYTE PTR [ecx], bl
+	mov ebx, 201
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 187
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+	mov ecx, OFFSET game131
+	mov ebx, 72
+	add ecx, 34
+	mov BYTE PTR [ecx], bl
+	mov ebx, 186
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 186
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+	mov ecx, OFFSET game132
+	mov ebx, 205
+	add ecx, 34
+	mov BYTE PTR [ecx], bl
+	mov ebx, 204
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 185
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+	mov ecx, OFFSET game133
+	mov ebx, 205
+	add ecx, 34
+	mov BYTE PTR [ecx], bl
+	mov ebx, 200
+	add ecx, -1
+	mov BYTE PTR [ecx], bl
+	mov ebx, 188
+	add ecx, 2
+	mov BYTE PTR [ecx], bl
+end_draw:
+	ret
+draw_player ENDP
+
+draw_game PROC
+	xor ecx, ecx
 	call Clrscr
-	
-	;limpando a matriz da rua e colocando o carrinho na posição
-	
-Finish:
-	exit
-main ENDP
+	call Crlf
+	mov  edx,OFFSET game000
+    call WriteString
+	mov  edx,OFFSET game100
+    call WriteString
+	mov  edx,OFFSET game200
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game001
+    call WriteString
+	mov  edx,OFFSET game101
+    call WriteString
+	mov  edx,OFFSET game201
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game002
+    call WriteString
+	mov  edx,OFFSET game102
+    call WriteString
+	mov  edx,OFFSET game202
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game003
+    call WriteString
+	mov  edx,OFFSET game103
+    call WriteString
+	mov  edx,OFFSET game203
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game004
+    call WriteString
+	mov edx, OFFSET game104
+	call WriteString
+	mov  edx,OFFSET game204
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game005
+    call WriteString
+	mov  edx,OFFSET game105
+    call WriteString
+	mov  edx,OFFSET game205
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game006
+    call WriteString
+	mov  edx,OFFSET game106
+    call WriteString
+	mov  edx,OFFSET game206
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game007
+    call WriteString
+	mov  edx,OFFSET game107
+    call WriteString
+	mov  edx,OFFSET game207
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game008
+    call WriteString
+	mov  edx,OFFSET game108
+    call WriteString
+	mov  edx,OFFSET game208
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game009
+    call WriteString
+	mov edx, OFFSET game109
+	call WriteString
+	mov  edx,OFFSET game209
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game010
+    call WriteString
+	mov  edx,OFFSET game110
+    call WriteString
+	mov  edx,OFFSET game210	
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game011
+    call WriteString
+	mov  edx,OFFSET game111
+    call WriteString
+	mov  edx,OFFSET game211
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game012
+    call WriteString
+	mov  edx,OFFSET game112
+    call WriteString
+	mov  edx,OFFSET game212
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game013
+    call WriteString
+	mov  edx,OFFSET game113
+    call WriteString
+	mov  edx,OFFSET game213
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game014
+    call WriteString
+	mov edx, OFFSET game114
+	call WriteString
+	mov  edx,OFFSET game214
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game015
+    call WriteString
+	mov  edx,OFFSET game115
+    call WriteString
+	mov  edx,OFFSET game215
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game016
+    call WriteString
+	mov  edx,OFFSET game116
+    call WriteString
+	mov  edx,OFFSET game216
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game017
+    call WriteString
+	mov  edx,OFFSET game117
+    call WriteString
+	mov  edx,OFFSET game217
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game018
+    call WriteString
+	mov  edx,OFFSET game118
+    call WriteString
+	mov  edx,OFFSET game218
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game019
+    call WriteString
+	mov edx, OFFSET game119
+	call WriteString
+	mov  edx,OFFSET game219
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game020
+    call WriteString
+	mov  edx,OFFSET game120
+    call WriteString
+	mov  edx,OFFSET game220
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game021
+    call WriteString
+	mov  edx,OFFSET game121
+    call WriteString
+	mov  edx,OFFSET game221
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game022
+    call WriteString
+	mov  edx,OFFSET game122
+    call WriteString
+	mov  edx,OFFSET game222
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game023
+    call WriteString
+	mov  edx,OFFSET game123
+    call WriteString
+	mov  edx,OFFSET game223
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game024
+    call WriteString
+	mov edx, OFFSET game124
+	call WriteString
+	mov  edx,OFFSET game224
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game025
+    call WriteString
+	mov  edx,OFFSET game125
+    call WriteString
+	mov  edx,OFFSET game225
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game026
+    call WriteString
+	mov  edx,OFFSET game126
+    call WriteString
+	mov  edx,OFFSET game226
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game027
+    call WriteString
+	mov  edx,OFFSET game127
+    call WriteString
+	mov  edx,OFFSET game227
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game028
+    call WriteString
+	mov  edx,OFFSET game128
+    call WriteString
+	mov  edx,OFFSET game228
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game029
+    call WriteString
+	mov edx, OFFSET game129
+	call WriteString
+	mov  edx,OFFSET game229
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game030
+    call WriteString
+	mov  edx,OFFSET game130
+    call WriteString
+	mov  edx,OFFSET game230
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game031
+    call WriteString
+	mov  edx,OFFSET game131
+    call WriteString
+	mov  edx,OFFSET game231
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game032
+    call WriteString
+	mov  edx,OFFSET game132
+    call WriteString
+	mov  edx,OFFSET game232
+    call WriteString
+	call Crlf
+	mov  edx,OFFSET game033
+    call WriteString
+	mov  edx,OFFSET game133
+    call WriteString
+	mov  edx,OFFSET game233
+    call WriteString
+	call Crlf
+	ret
+draw_game ENDP
 END main
+
